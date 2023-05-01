ea21c5cc647f2bbe30fbd63ae87a725dd2c55fbf
Farid Azizov <farid_azizov@protonmail.com>
2023-05-01 10:57:56 +0300

    deleting password

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 /* Database connection start */
-$servername = "anysql.itcollege.ee";
-$username = "ICS0008_WT_19";
-$password = "9913c37187f6";
+$servername = "your_servername";
+$username = "your_username";
+$password = "your_password";
 $dbname = "ICS0008_19";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
